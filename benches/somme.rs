b3d8b17c6978013c238f855e73ab1627283bb25b
Sarabac <lucas.boutar@gmail.com>
2023-02-26 21:30:47 +0100

    Bench Test

---
+++ benches/somme.rs
@@ -0,0 +1,14 @@
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use testrust::fibo::{fibonacci, fibo2};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+}
+
+pub fn crit_fibo2(c: &mut Criterion) {
+    c.bench_function("fibo encore", |b|b.iter(|| fibo2(black_box(20))));
+}
+
+criterion_group!(benches, criterion_benchmark, crit_fibo2);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod chaine;
 pub mod composite;
 pub mod testage;
 pub mod threadtest;
+pub mod fibo;
+pub mod expression;
